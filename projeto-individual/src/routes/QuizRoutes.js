37bb269ed3f7c1d56331991ea98042c5924e8251
GabrielaPereiraSantana <gabriela.santana@sptech.school>
2025-12-03 21:20:30 -0300

    adicionando icon em todas as telas

--- projeto-individual/src/routes/QuizRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const quizController = require("../controllers/QuizController")
-const router = express.Router()
-
-router.get("/get-quiz/:id", quizController.getQuiz)
-
-
-module.exports = router
--- projeto-individual/src/routes/ScoreRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const scoreController = require("../controllers/ScoreController")
-const router = express.Router()
-
-router.post("/pontuar", scoreController.pontuar)
-router.get("/ranking", scoreController.ranking)
-
-module.exports = router
